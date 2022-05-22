9d84a956ae6ad85f891af742cf3ad7dcd5e1f09d
PMBytner <pawel.m.bytner@gmail.com>
2022-05-22 13:07:11 +0200

    cleanup i szkielet okna głównego z listą osób

--- Model.cs
+++ Model.cs
@@ -8,68 +8,8 @@ using System.Threading.Tasks;
 
 namespace NET_ININ3_PR2_z1
 {
-    class Osoba : INotifyPropertyChanged
+    class Model
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        readonly static Dictionary<string, string[]> powiązaneWłaściwości = new Dictionary<string, string[]>()
-        {
-            ["Imię"] = new string[] { "ImięNazwisko" },
-            ["Nazwisko"] = new string[] {"ImięNazwisko"}
-        };
-        public void OnPropertyChanged([CallerMemberName] string własnaNazwa = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(własnaNazwa));
-            foreach (string nazwaPowiązana in powiązaneWłaściwości[własnaNazwa])
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nazwaPowiązana));
-        }
-        public static uint następneID = 0;
-
-        public uint ID { get; } = następneID++;
-        string
-            imię,
-            nazwisko
-            ;
-        public string ImięNazwisko
-        {
-            get { return $"{Imię} {Nazwisko}"; }
-        }
-
-        public string Imię {
-            get => imię;
-            set {
-                imię = value;
-                OnPropertyChanged();
-            }
-        }
-        public string Nazwisko {
-            get => nazwisko;
-            set
-            {
-                nazwisko = value;
-                OnPropertyChanged();
-            }
-        }
-    }
-    class Model : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        readonly static Dictionary<string, string[]> powiązaneWłaściwości = new Dictionary<string, string[]>()
-        {
-            ["Imię"] = new string[] { "Format" }
-        };
-        public void OnPropertyChanged([CallerMemberName] string własnaNazwa = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(własnaNazwa));
-            foreach (string nazwaPowiązana in powiązaneWłaściwości[własnaNazwa])
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nazwaPowiązana));
-        }
-
-        public string[] Tab { get; set; } = new string[]
-        {
-            "a",
-            "b",
-            "c"
-        };
         public LinkedList<Osoba> ListaOsób { get; set; } = new LinkedList<Osoba>(new Osoba[]{
             new Osoba(){Imię="Jan", Nazwisko="Sosnowski"},
             new Osoba(){Imię="Adam",Nazwisko="Dąbrowski"},
---
+++ Osoba.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_ININ3_PR2_z1
+{
+    class Osoba : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        readonly static Dictionary<string, string[]> powiązaneWłaściwości = new Dictionary<string, string[]>()
+        {
+            ["Imię"] = new string[] { "ImięNazwisko" },
+            ["Nazwisko"] = new string[] { "ImięNazwisko" }
+        };
+        public void OnPropertyChanged([CallerMemberName] string własnaNazwa = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(własnaNazwa));
+            foreach (string nazwaPowiązana in powiązaneWłaściwości[własnaNazwa])
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nazwaPowiązana));
+        }
+
+        public static uint następneID = 0;
+
+        public uint ID { get; } = następneID++;
+        string
+            imię,
+            nazwisko
+            ;
+        public string ImięNazwisko
+        {
+            get { return $"{Imię} {Nazwisko}"; }
+        }
+
+        public string Imię
+        {
+            get => imię;
+            set
+            {
+                imię = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Nazwisko
+        {
+            get => nazwisko;
+            set
+            {
+                nazwisko = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public override string ToString()
+        {
+            return ImięNazwisko;
+        }
+    }
+}
